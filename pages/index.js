95ea3ec39b4e71c2f256db98027aa0530c5d55c7
Elias Johansson <elias_johansson@hotmail.se>
2019-03-26 15:11:41 +0100

    Fixed event query parameters

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import qs from 'query-string';
 
 const StyledIndex = styled.div`
   background-color: ${({ theme }) => theme.backgroundColor};
@@ -37,10 +38,24 @@ const Index = props => (
 );
 
 Index.getInitialProps = async ({ req }) => {
+  const params = {
+    access_token: process.env.FB_ACCESS_TOKEN,
+    fields: [
+      'cover',
+      'description',
+      'name',
+      'owner',
+      'start_time',
+      'end_time',
+      'category'
+    ]
+  };
+
   const res = await fetch(
-    `https://graph.facebook.com/v3.2/421066311976162/events?access_token=${
-      process.env.FB_ACCESS_TOKEN
-    }`
+    `https://graph.facebook.com/v3.2/421066311976162/events?${qs.stringify(
+      params,
+      { arrayFormat: 'comma' }
+    )}`
   );
   const json = await res.json();
   return { events: json };
