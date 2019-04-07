4aaedd263d0e26906a8d4bbbe0b24603edc9455b
Elias Johansson <elias_johansson@hotmail.se>
2019-04-07 12:06:58 +0200

    Storybook styled-components setup

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,24 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyles from '../styles/GlobalStyles';
+import Theme from '../styles/Theme';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(storyFn => (
+  <ThemeProvider theme={Theme}>
+    <>
+      <Normalize />
+      <GlobalStyles />
+
+      {storyFn()}
+    </>
+  </ThemeProvider>
+));
 
 configure(loadStories, module);
