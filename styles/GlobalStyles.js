edcbeca6d62dfed9ee42efe1ced0547c7ba35039
Elias Johansson <elias_johansson@hotmail.se>
2019-04-07 13:11:31 +0200

    Global styling

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -1,12 +1,54 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto:400,700');
+
   *,*::before,*::after {
     box-sizing: border-box;
   }
+  
   body {
-    background-color: rebeccapurple;
+    font-family: 'Roboto', sans-serif;
+    background-color: ${({ theme }) => theme.colorLightGrey};
   }
+
+  h1 {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 32px;
+    font-weight: bold;
+  }
+
+  h2 {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  h3 {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  p {
+    font-size: 16px;
+    font-weight: normal;
+  }
+
+  b {
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  small {
+    font-size: 11px;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
 `;
 
 export default GlobalStyle;
