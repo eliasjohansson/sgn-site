eecff3d99d5b92a1968d32e675590b4c847c3f43
Elias Johansson <elias_johansson@hotmail.se>
2019-04-07 13:12:17 +0200

    Navbar start

---
+++ components/Container.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 1rem;
+  margin: 0 auto;
+
+  @media screen and (min-width: 1190px) {
+    max-width: 1190px;
+  }
+`;
+
+export default Container;
--- styles/Theme.js
+++ styles/Theme.js
@@ -1,5 +1,9 @@
 const Theme = {
-  backgroundColor: 'tomato'
+  colorPrimary: '#1BB9A6',
+  colorWhite: '#fff',
+  colorLightGrey: '#F6F6F7',
+  colorGrey: '#D2D2D2',
+  colorDarkGrey: '#393939'
 };
 
 export default Theme;
