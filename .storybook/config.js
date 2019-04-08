ab1485fbcf84892972e509b51ba3cbb83a515cc9
Elias Johansson <elias_johansson@hotmail.se>
2019-04-08 12:36:59 +0200

    Added storybook viewport addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { configure, addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
+import { addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import GlobalStyles from '../styles/GlobalStyles';
 import Theme from '../styles/Theme';
+
+addParameters({ viewport: { viewports: { ...INITIAL_VIEWPORTS } } });
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
