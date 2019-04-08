9d8408d4b1286fab28f676d0211cebac3248b019
Elias Johansson <elias_johansson@hotmail.se>
2019-04-08 22:46:35 +0200

    Input component with stories

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
-import { configure, addDecorator } from "@storybook/react";
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { configure, addDecorator } from '@storybook/react';
 
-import Button from "../components/Button.js";
+import Button from '../components/Button.js';
 
 addDecorator(storyFn => (
-  <div style={{ height: "100vh", backgroundColor: "#e5e5e5" }}>{storyFn()}</div>
+  <div style={{ height: '100vh', backgroundColor: '#e5e5e5' }}>{storyFn()}</div>
 ));
-storiesOf("Button", module).add("CTA", () => (
+
+storiesOf('Button', module).add('CTA', () => (
   <div>
-    <Button onClick={action("clicked")} />
-    <Button primary onClick={action("clicked")} />
-    <Button outlined onClick={action("clicked")} />
-    <Button primary small onClick={action("clicked")} />
+    <Button onClick={action('clicked')} />
+    <Button primary onClick={action('clicked')} />
+    <Button outlined onClick={action('clicked')} />
+    <Button primary small onClick={action('clicked')} />
   </div>
 ));
---
+++ stories/Forms.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { addDecorator } from '@storybook/react';
+
+import Container from '../components/Container';
+import Input from '../components/Input.js';
+import Button from '../components/Button';
+
+addDecorator(storyFn => (
+  <Container style={{ paddingTop: '2rem' }}>{storyFn()}</Container>
+));
+
+storiesOf('Forms', module)
+  .add('input', () => <Input />)
+  .add('input with label', () => <Input label="First Name" />)
+  .add('input with description', () => (
+    <Input label="Person Number" description="(YYYY-MM-DD-000)" />
+  ))
+  .add('textarea', () => <Input textarea label="Message" />)
+  .add('Example "Contact Us"', () => (
+    <div>
+      <Input label="Name" />
+      <Input label="Email" type="email" />
+      <Input label="Subject" />
+      <Input textarea label="Message" />
+      <Button>Send</Button>
+    </div>
+  ));
