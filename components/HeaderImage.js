f91f14e89386b478bc15508d85dd83495b3eb33f
Nils <nilsnathorstwindahl@gmail.com>
2019-04-17 17:19:29 +0200

    added membership page

--- components/HeaderImage.js
+++ components/HeaderImage.js
@@ -2,11 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledHeaderImage = styled.div`
+  /* position: relative; */
   width: 100vw;
+  height: 50vh;
   img {
     object-fit: cover;
+    object-position: 50% 50%;
+    height: 100%;
     width: 100%;
-    max-height: 50vh;
   }
   @media screen and (max-width: 900px) {
     img {
@@ -17,8 +20,7 @@ const StyledHeaderImage = styled.div`
 const HeaderImage = props => {
   return (
     <StyledHeaderImage>
-      <img src="https://via.placeholder.com/1440x880" />
-      <h1>Hello</h1>
+      <img src="https://images.pexels.com/photos/106829/pexels-photo-106829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
     </StyledHeaderImage>
   );
 };
