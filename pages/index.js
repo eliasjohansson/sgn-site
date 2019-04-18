6ed29e865006d9dc89c1d22f4545fe240cc5f220
Elias Johansson <elias_johansson@hotmail.se>
2019-04-18 17:00:11 +0200

    Enabled grahpql file extension imports

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import qs from 'query-string';
 
+// Queries
+import HOME_QUERY from '../graphql/home.gql';
+
 // Components
 import BigSection from '../components/Home/BigSection';
 import Banner from '../components/Banner';
@@ -20,37 +23,9 @@ const StyledIndex = styled.div`
   background-color: ${({ theme }) => theme.backgroundColor};
 `;
 
-export const HOME_QUERY = gql`
-  query homeQuery($lang: String!) {
-    page: pages(where: { title: "Home", lang: $lang }) {
-      edges {
-        node {
-          home {
-            header {
-              title
-              image
-            }
-            card1 {
-              image {
-                url
-              }
-              title
-              text
-            }
-            quote
-            card2 {
-              image {
-                url
-              }
-              title
-              text
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+/* export const HOME_QUERY = gql`
+
+`; */
 
 const Index = props => {
   const { lang } = props;
@@ -65,39 +40,29 @@ const Index = props => {
 
           return (
             <>
-              <Hero image="https://images.unsplash.com/photo-1548107116-762a8ff356f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80" />
-              <BigSection image="https://images.unsplash.com/photo-1511632765486-a01980e01a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80">
-                <h1>H1 - Desktop</h1>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Molestias optio laboriosam perspiciatis, debitis aliquid et,
-                  iste esse velit, iusto facere perferendis tempora illo
-                  dignissimos consequatur est inventore porro laborum sapiente.
-                </p>
+              <Hero image={page.header.image} />
+              <BigSection image={page.card1.image}>
+                <h1>{page.card1.title}</h1>
+                <p>{page.card1.text}</p>
               </BigSection>
               <Banner>
-                <i>
-                  “De har hjälpt mig att hålla mig igång och uppleva
-                  meningsfullhet i den riktningslösa framtiden”.
-                </i>
+                <i>“{page.quote}”.</i>
               </Banner>
-              <BigSection />
+              <BigSection image={page.card2.image}>
+                <h1>{page.card2.title}</h1>
+                <p>{page.card2.text}</p>
+              </BigSection>
               <Visions />
               <Banner primary>
-                <h1>Title</h1>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas
-                  nisi dicta quisquam aut dolor, assumenda commodi mollitia
-                  voluptatem amet ea, nobis quasi repudiandae repellendus
-                  nostrum quidem. Vitae quod non eaque!
-                </p>
+                <h1>{page.green_banner_1.title}</h1>
+                <p>{page.green_banner_1.text}</p>
                 <Button>CTA</Button>
               </Banner>
               <Projects />
               <Awards />
               <News />
               <Banner primary>
-                <h1>Title</h1>
+                <h1>{page.green_banner_2.title}</h1>
                 <Button>CTA</Button>
               </Banner>
               <Partners />
