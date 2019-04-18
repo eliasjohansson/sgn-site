47282608d1f80b384bc8775a7c809b604f27ba9a
Nils <nilsnathorstwindahl@gmail.com>
2019-04-18 13:48:08 +0200

    added collaborations page

--- pages/collaborations.js
+++ pages/collaborations.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Collabs from "../components/Collaborations";
+import HeaderImage from "../components/HeaderImage";
+import Section from "../components/Section";
+import Banner from "../components/Banner";
+import Button from "../components/Button";
 
-const StyledCollaborations = styled.div``;
-
+const StyledCollaborations = styled.div`
+  & h1 {
+    margin: 0;
+    margin-bottom: 5rem;
+  }
+`;
+const Header = styled(Section)`
+  background-color: ${({ theme }) => theme.colorWhite};
+  color: ${({ theme }) => theme.colorDarkGrey};
+`;
 const Collaborations = props => {
   return (
     <StyledCollaborations>
-      <h1>Collaborations</h1>
-      <p>{props.lang}</p>
+      <HeaderImage />
+      <Header>
+        <h1>Headline</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
+          Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.
+          Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
+        </p>
+      </Header>
+      <Collabs />
+      <Banner primary>
+        <h1>Title</h1>
+        <Button>CTA</Button>
+      </Banner>
     </StyledCollaborations>
   );
 };
--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
-const routes = require('next-routes');
+const routes = require("next-routes");
 
-const lang = ':lang(en|sv)';
+const lang = ":lang(en|sv)";
 
 module.exports = routes()
-  .add('index', `/${lang}`)
-  .add('news', `/${lang}/news`)
-  .add('membership', `/${lang}/membership`)
-  .add('collaborations', `/${lang}/collaborations`)
-  .add('donations', `/${lang}/donations`)
-  .add('branches', `/${lang}/branches`);
+  .add("index", `/${lang}`)
+  .add("news", `/${lang}/news`)
+  .add("membership", `/${lang}/membership`)
+  .add("collaborations", `/${lang}/collaborations`)
+  .add("donate", `/${lang}/donate`)
+  .add("branches", `/${lang}/branches`);
 /* .add('branch', `/${lang}/branch/:id`); */
