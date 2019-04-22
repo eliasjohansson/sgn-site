c27b18dd68114efed275756d5720882f92a316a3
Elias Johansson <elias_johansson@hotmail.se>
2019-04-22 22:34:54 +0200

    Created collaborations page query

--- pages/collaborations.js
+++ pages/collaborations.js
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'next/router';
 
+// Queries
+import COLLABORATIONS_QUERY from '../graphql/collaborations.gql';
+
 // Components
 import Collabs from '../components/Collaborations';
 import HeaderImage from '../components/HeaderImage';
 import Section from '../components/Section';
 import Banner from '../components/Banner';
 import Button from '../components/Button';
+import { Query } from 'react-apollo';
 
 const StyledCollaborations = styled.div`
   & h1 {
@@ -20,25 +24,38 @@ const Header = styled(Section)`
   color: ${({ theme }) => theme.colorDarkGrey};
 `;
 
-const Collaborations = withRouter(props => {
-  return (
-    <StyledCollaborations>
-      <HeaderImage image="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-      <Header>
-        <h1>Headline</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
-          Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.
-          Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-        </p>
-      </Header>
-      <Collabs lang={props.lang} />
-      <Banner primary>
-        <h1>Title</h1>
-        <Button>CTA</Button>
-      </Banner>
-    </StyledCollaborations>
-  );
-});
+const Collaborations = ({ lang }) => (
+  <StyledCollaborations>
+    <Query query={COLLABORATIONS_QUERY} variables={{ lang: lang }}>
+      {({ loading, error, data, fetchMore }) => {
+        let page;
+        if (!loading) {
+          page = data.page.edges[0].node.collaborations;
+        } else {
+          return null;
+        }
+        return (
+          <>
+            <HeaderImage image={page.header.image} />
+            <Header>
+              <h1>Headline</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+                odio. Quisque volutpat mattis eros. Nullam malesuada erat ut
+                turpis. Suspendisse urna nibh, viverra non, semper suscipit,
+                posuere a, pede.
+              </p>
+            </Header>
+            <Collabs lang={lang} />
+            <Banner primary>
+              <h1>Title</h1>
+              <Button>CTA</Button>
+            </Banner>
+          </>
+        );
+      }}
+    </Query>
+  </StyledCollaborations>
+);
 
 export default Collaborations;
